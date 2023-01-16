72803aa02d779fa94484476193770d89cc51de40
amJayem <csejayem19@gmail.com>
2023-01-16 16:55:47 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-const express = require( 'express' )
+const express = require( 'express' );
 const app = express();
 const cors = require( 'cors' );
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require( 'dotenv' ).config();
-
 
 // middleware
 app.use( cors() );
 app.use( express.json() );
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pl2ayam.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+ async function run(){
+    try{
+    const test = client.db('techQuest').collection('tes');
+        app.get("/allJobs", async (req, res) => {
+            const result = await test.find({}).toArray();
+            // console.log(result);
+            res.send(result);
+          });
+    }
+    catch{e=>{
+        console.error('error inside run function: ',e);
+    }}
+}
+
+run().catch(e=> console.log('run function error..',e))
+
 app.get( '/', ( req, res ) => {
-    res.send( 'Tech Quest server is running' )
+    res.send( 'Tech Quest server is running...' );
 } )
 
 app.listen( port, () => {
