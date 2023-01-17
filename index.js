81ecf831732c6044fc223b423c755e9ef1f61c27
Mahfuj-HSTU <rmahfujur16@gmail.com>
2023-01-17 10:04:41 +0600

    users added

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
-const express = require('express');
+const express = require( 'express' );
 const app = express();
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require( 'cors' );
+const { MongoClient, ServerApiVersion } = require( 'mongodb' );
 const port = process.env.PORT || 5000;
-require('dotenv').config();
+require( 'dotenv' ).config();
 
 // middleware
-app.use(cors());
-app.use(express.json());
+app.use( cors() );
+app.use( express.json() );
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pl2ayam.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASS }@cluster0.pl2ayam.mongodb.net/?retryWrites=true&w=majority`;
+console.log( uri )
+const client = new MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 } );
 
-async function run() {
+async function run () {
     try {
-        const test = client.db('techQuest').collection('tes');
-        app.get("/allJobs", async (req, res) => {
-            const result = await test.find({}).toArray();
+        const test = client.db( 'techQuest' ).collection( 'tes' );
+        const usersCollection = client.db( 'techQuest' ).collection( 'users' );
+
+
+        app.get( "/allJobs", async ( req, res ) => {
+            const result = await test.find( {} ).toArray();
             // console.log(result);
-            res.send(result);
-        });
+            res.send( result );
+        } );
+
+        // post users
+        app.post( '/users', async ( req, res ) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne( user );
+            res.send( result )
+        } )
     }
     catch {
         e => {
-            console.error('error inside run function: ', e);
+            console.error( 'error inside run function: ', e );
         }
     }
 }
 
-run().catch(e => console.log('run function error..', e))
+run().catch( e => console.log( 'run function error..', e ) )
 
-app.get('/', (req, res) => {
-    res.send('Tech Quest server is running...');
-})
+app.get( '/', ( req, res ) => {
+    res.send( 'Tech Quest server is running...' );
+} )
 
-app.listen(port, () => {
-    console.log(`Tech Quest server is running on ${port}`);
-})
+app.listen( port, () => {
+    console.log( `Tech Quest server is running on ${ port }` );
+} )
