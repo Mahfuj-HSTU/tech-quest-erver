c9851085aa9c3827522eb3bd671136adbda5a37e
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2023-01-19 21:52:21 +0600

    update by omar

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const cors = require( 'cors' );
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-require( 'dotenv' ).config();
-
+require("dotenv").config();
 
 // middleware
-app.use( cors() );
-app.use( express.json() );
+app.use(cors());
+app.use(express.json());
 
-app.get( '/', ( req, res ) => {
-    res.send( 'Tech Quest server is running' )
-} )
+app.get("/", (req, res) => {
+  res.send("Tech Quest server is running");
+});
 
-app.listen( port, () => {
-    console.log( `Tech Quest server is running on ${ port }`);
-} )
+app.listen(port, () => {
+  console.log(`Tech Quest server is running on ${port}`);
+});
