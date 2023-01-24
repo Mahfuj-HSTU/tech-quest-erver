640ae6335dc87a2293af66840118ab15a221ebcd
Mahfuj-HSTU <rmahfujur16@gmail.com>
2023-01-24 23:12:24 +0600

    check

--- index.js
+++ index.js
@@ -1,117 +1,111 @@
-const express = require("express");
+const express = require( "express" );
 const app = express();
-const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require( "cors" );
+const { MongoClient, ServerApiVersion } = require( "mongodb" );
 const port = process.env.PORT || 5000;
-require("dotenv").config();
+require( "dotenv" ).config();
 
 // middleware
-app.use(cors());
-app.use(express.json());
+app.use( cors() );
+app.use( express.json() );
 
 const courses = require( './data/courses.json' )
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pl2ayam.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASS }@cluster0.pl2ayam.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, {
+const client = new MongoClient( uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
-});
+} );
 
-async function run() {
+async function run () {
   try {
     // const test = client.db('techQuest').collection('test');
-    const usersCollection = client.db("techQuest").collection("users");
+    const usersCollection = client.db( "techQuest" ).collection( "users" );
     const recruiterJobPostsCollection = client
-      .db("techQuest")
-      .collection("recruiterJobPosts");
+      .db( "techQuest" )
+      .collection( "recruiterJobPosts" );
     const applicationCollection = client
-      .db("techQuest")
-      .collection("applications");
+      .db( "techQuest" )
+      .collection( "applications" );
 
-    app.get("/allJobs", async (req, res) => {
-      const result = await test.find({}).toArray();
+    app.get( "/allJobs", async ( req, res ) => {
+      const result = await test.find( {} ).toArray();
       // console.log(result);
-      res.send(result);
-    });
+      res.send( result );
+    } );
 
     // Posts recruiters
-    app.get("/recruiterJobPosts", async (req, res) => {
+    app.get( "/recruiterJobPosts", async ( req, res ) => {
       const query = {};
-      const result = await recruiterJobPostsCollection.find(query).toArray();
-      res.send(result);
-    });
+      const result = await recruiterJobPostsCollection.find( query ).toArray();
+      res.send( result );
+    } );
 
     // post users
-    app.post("/users", async (req, res) => {
+    app.post( "/users", async ( req, res ) => {
       const user = req.body;
-      const result = await usersCollection.insertOne(user);
-      res.send(result);
-    });
+      const result = await usersCollection.insertOne( user );
+      res.send( result );
+    } );
 
     // storing job seekers application
-    app.post("/applications", async (req, res) => {
+    app.post( "/applications", async ( req, res ) => {
       const application = req.body;
       // console.log(application);
-      const result = await applicationCollection.insertOne(application);
+      const result = await applicationCollection.insertOne( application );
       // console.log(result);
-      res.send(result);
-    });
+      res.send( result );
+    } );
 
-<<<<<<< HEAD
-        // get recruiter
-        // check buyer
-        app.get( '/users/recruiter/:email', async ( req, res ) => {
-            const email = req.params.email
-            const query = { email }
-            // console.log( email );
-            const user = await usersCollection.findOne( query )
-            res.send( { isRecruiter: user?.role === 'recruiter' } )
-        } )
+    // get recruiter
+    app.get( '/users/recruiter/:email', async ( req, res ) => {
+      const email = req.params.email
+      const query = { email }
+      // console.log( email );
+      const user = await usersCollection.findOne( query )
+      res.send( { isRecruiter: user?.role === 'recruiter' } )
+    } )
 
-        // check jobSeeker
-        app.get( '/users/jobSeeker/:email', async ( req, res ) => {
-            const email = req.params.email
-            const query = { email }
-            // console.log( email );
-            const user = await usersCollection.findOne( query )
-            res.send( { isJobSeeker: user?.role === 'jobSeeker' } )
-        } )
+    // check jobSeeker
+    app.get( '/users/jobSeeker/:email', async ( req, res ) => {
+      const email = req.params.email
+      const query = { email }
+      // console.log( email );
+      const user = await usersCollection.findOne( query )
+      res.send( { isJobSeeker: user?.role === 'jobSeeker' } )
+    } )
 
-        app.get( '/courses', ( req, res ) => {
-            res.send( courses )
-        } )
-=======
     // getting all application from db
-    app.get("/applications", async (req, res) => {
-      const result = await applicationCollection.find({}).toArray();
-      res.send(result);
-    });
->>>>>>> b28f54aabc2c06296367f06b00f25c1569df244c
+    app.get( "/applications", async ( req, res ) => {
+      const result = await applicationCollection.find( {} ).toArray();
+      res.send( result );
+    } );
 
-    app.get("/courses", (req, res) => {
-      res.send(courses);
-    });
+    // courses
+    app.get( "/courses", ( req, res ) => {
+      res.send( courses );
+    } );
 
-    app.get("/courses/:id", (req, res) => {
+    app.get( "/courses/:id", ( req, res ) => {
       const id = req.params.id;
-      const selectedCourse = courses.find((course) => course.id === id);
-      res.send(selectedCourse);
-    });
+      const selectedCourse = courses.find( ( course ) => course.id === id );
+      res.send( selectedCourse );
+    } );
   } catch {
-    (e) => {
-      console.error("error inside run function: ", e);
+    ( e ) => {
+      console.error( "error inside run function: ", e );
     };
   }
 }
 
-run().catch((e) => console.error("run function error..", e));
+run().catch( ( e ) => console.error( "run function error..", e ) );
 
-app.get("/", (req, res) => {
-  res.send("Tech Quest server is running...");
-});
+app.get( "/", ( req, res ) => {
+  res.send( "Tech Quest server is running..." );
+} );
 
-app.listen(port, () => {
-  console.log(`Tech Quest server is running on ${port}`);
-});
+app.listen( port, () => {
+  console.log( `Tech Quest server is running on ${ port }` );
+} );
