9610b03af1eee4226ff742e67e5b182d45c53c93
siam50 <siamurrahman50@gmail.com>
2023-02-03 20:04:15 +0600

    correction recriter code

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require( "express" );
+const express = require("express");
 const app = express();
-const cors = require( "cors" );
-const { MongoClient, ServerApiVersion, ObjectId } = require( "mongodb" );
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 
 // middleware
-app.use( cors() );
-app.use( express.json() );
+app.use(cors());
+app.use(express.json());
 // const courses = require("./data/courses.json");
 
-const uri = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASS }@cluster0.pl2ayam.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pl2ayam.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient( uri, {
+const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
-} );
+});
 
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
@@ -39,30 +39,30 @@ function verifyJWT(req, res, next) {
 
 async function run() {
   try {
-    const usersCollection = client.db( "techQuest" ).collection( "users" );
-    const allJobsCollection = client.db( "techQuest" ).collection( "recruiterJobPosts" );
-    const recruiterJobPostsCollection = client.db( "techQuest" ).collection( "recruiterJobPosts" );
-    const applicationCollection = client.db( "techQuest" ).collection( "applications" );
-    const jobSeekersCollection = client.db( "techQuest" ).collection( "jobSeekersCollection" );
-    const courseCollection = client.db( "techQuest" ).collection( "courses" );
-    const test = client.db( "techQuest" ).collection( "test" ); // created by jayem for testing
+    const usersCollection = client.db("techQuest").collection("users");
+    const allJobsCollection = client.db("techQuest").collection("recruiterJobPosts");
+    const recruiterJobPostsCollection = client.db("techQuest").collection("recruiterJobPosts");
+    const applicationCollection = client.db("techQuest").collection("applications");
+    const jobSeekersCollection = client.db("techQuest").collection("jobSeekersCollection");
+    const courseCollection = client.db("techQuest").collection("courses");
+    const test = client.db("techQuest").collection("test"); // created by jayem for testing
 
     // Create post method for add job section
-    app.post( "/alljobs", async ( req, res ) => {
+    app.post("/alljobs", async (req, res) => {
       const jobPostDetails = req.body;
-      const result = await allJobsCollection.insertOne( jobPostDetails );
+      const result = await allJobsCollection.insertOne(jobPostDetails);
       // console.log( result );
-      res.send( result );
-    } );
+      res.send(result);
+    });
 
     // deleting job by id
-    app.delete( '/delete-job/:id', async ( req, res ) => {
+    app.delete('/delete-job/:id', async (req, res) => {
       const id = req.params.id;
       // console.log(id);
-      const filter = { _id: ObjectId( id ) };
-      const result = await recruiterJobPostsCollection.deleteOne( filter );
-      res.send( result );
-    } )
+      const filter = { _id: ObjectId(id) };
+      const result = await recruiterJobPostsCollection.deleteOne(filter);
+      res.send(result);
+    })
 
     // my jobs
     app.get("/myjobs", verifyJWT, async (req, res) => {
@@ -76,10 +76,10 @@ async function run() {
       // }
 
       const query = { email: email };
-      const jobs = await applicationCollection.find( query ).toArray();
+      const jobs = await applicationCollection.find(query).toArray();
       // console.log( result );
-      res.send( jobs );
-    } );
+      res.send(jobs);
+    });
 
     // getting a specific job
     app.get("/applications/:id", async (req, res) => {
@@ -87,26 +87,18 @@ async function run() {
       //   console.log( id );
       const filter = { _id: ObjectId(id) };
       const result = await applicationCollection.findOne(filter);
-        console.log( result );
+      console.log(result);
       res.send(result);
     });
 
-    // recruiter job posts
-    app.get( "/recruiterJobPosts", async ( req, res ) => {
+    app.get("/jobSeekersCollection", async (req, res) => {
       const query = {};
-      const result = await recruiterJobPostsCollection.find( query ).toArray();
-      // const result = await test.find(query).toArray();
-      res.send( result );
-    } );
-
-    app.get( "/jobSeekersCollection", async ( req, res ) => {
-      const query = {};
-      const result = await jobSeekersCollection.find( query ).toArray();
-      res.send( result );
-    } );
+      const result = await jobSeekersCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // post users
-    app.post( "/users", async ( req, res ) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
       res.send(result);
@@ -114,7 +106,7 @@ async function run() {
     });
 
     // Update Users Profile
-    app.put( '/users/:email', async ( req, res ) => {
+    app.put('/users/:email', async (req, res) => {
       const email = req.params.email;
       const editProfile = req.body;
       const { name, PresentAddress, ParmanentAddress, mobile } = editProfile;
@@ -129,27 +121,27 @@ async function run() {
           mobile
         }
       }
-      const result = await usersCollection.updateOne( filter, updateDoc, options );
-      res.send( result );
-    } );
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
 
     // storing job seekers application
-    app.post( "/applications", async ( req, res ) => {
+    app.post("/applications", async (req, res) => {
       const application = req.body;
-      const result = await applicationCollection.insertOne( application );
-      res.send( result );
-    } );
+      const result = await applicationCollection.insertOne(application);
+      res.send(result);
+    });
 
 
     // created a search query - it is not complete
-    app.get( "/search/:title", async ( req, res ) => {
+    app.get("/search/:title", async (req, res) => {
       // const title = req.query;
       const title = req.params.title;
       // const country = req.params.country;
       //   console.log(title);
       const filter = { $search: { title } };
       const result = await recruiterJobPostsCollection
-        .aggregate( [
+        .aggregate([
           {
             $search: {
               index: "job_title",
@@ -161,62 +153,53 @@ async function run() {
               },
             },
           },
-        ] )
+        ])
         .toArray();
-      res.send( result );
-    } );
-
-
-    // // recruiter job posts
-    // app.get( "/recruiterJobPosts", async ( req, res ) => {
-    //   const query = {};
-    //   const result = await recruiterJobPostsCollection.find( query ).toArray();
-    //   // const result = await test.find(query).toArray();
-    //   res.send( result );
-    // } );
+      res.send(result);
+    });
 
     // Posts recruiters
-    app.get( "/recruiterJobPosts", async ( req, res ) => {
+    app.get("/recruiterJobPosts", async (req, res) => {
       const email = req.query.email;
       let query = {}
-      if ( email ) {
+      if (email) {
         query = { recruiterEmail: email };
       }
-      const result = await recruiterJobPostsCollection.find( query ).toArray();
-      res.send( result );
-    } );
+      const result = await recruiterJobPostsCollection.find(query).toArray();
+      res.send(result);
+    });
 
 
-    app.delete( '/recruiterJobPosts/:id', async ( req, res ) => {
+    app.delete('/recruiterJobPosts/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: ObjectId( id ) };
-      const result = await recruiterJobPostsCollection.deleteOne( filter );
-      res.send( result );
-    } )
+      const filter = { _id: ObjectId(id) };
+      const result = await recruiterJobPostsCollection.deleteOne(filter);
+      res.send(result);
+    })
 
     // getting a specific job
-    app.get( "/job-details/:id", async ( req, res ) => {
+    app.get("/job-details/:id", async (req, res) => {
       const id = req.params.id;
       //   console.log( id );
-      const filter = { _id: ObjectId( id ) };
-      const result = await recruiterJobPostsCollection.findOne( filter );
+      const filter = { _id: ObjectId(id) };
+      const result = await recruiterJobPostsCollection.findOne(filter);
       //   console.log( result );
-      res.send( result );
-    } );
+      res.send(result);
+    });
 
     // post users
-    app.post( "/users", async ( req, res ) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      const result = await usersCollection.insertOne( user );
-      res.send( result );
-    } );
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
     // storing job seekers application
-    app.post( "/applications", async ( req, res ) => {
+    app.post("/applications", async (req, res) => {
       const application = req.body;
-      const result = await applicationCollection.insertOne( application );
-      res.send( result );
-    } );
+      const result = await applicationCollection.insertOne(application);
+      res.send(result);
+    });
 
     app.get("/jwt", async (req, res) => {
       const email = req.query.email;
@@ -231,39 +214,39 @@ async function run() {
       res.status(401).send({ accessToken: "" });
     });
     // get all users
-    app.get( '/users', async ( req, res ) => {
-      const users = await usersCollection.find( {} ).toArray();
-      res.send( users )
-    } )
+    app.get('/users', async (req, res) => {
+      const users = await usersCollection.find({}).toArray();
+      res.send(users)
+    })
 
     // delete users
-    app.delete( '/users/:id', async ( req, res ) => {
+    app.delete('/users/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id: ObjectId( id ) }
-      const result = await usersCollection.deleteOne( query );
+      const query = { _id: ObjectId(id) }
+      const result = await usersCollection.deleteOne(query);
       // console.log( result )
-      res.send( result )
-    } )
+      res.send(result)
+    })
 
     // getting user to check role
-    app.get( "/users/:email", async ( req, res ) => {
+    app.get("/users/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email };
       // console.log( email );
-      const user = await usersCollection.findOne( query );
-      res.send( user );
-    } );
+      const user = await usersCollection.findOne(query);
+      res.send(user);
+    });
 
     // getting all application from db
-    app.get( "/applications", async ( req, res ) => {
-      const result = await applicationCollection.find( {} ).toArray();
-      res.send( result );
-    } );
+    app.get("/applications", async (req, res) => {
+      const result = await applicationCollection.find({}).toArray();
+      res.send(result);
+    });
 
     // storing course one by one
     app.post(
       "/add-course/:title/:description/:instructor/:img/:price",
-      async ( req, res ) => {
+      async (req, res) => {
         const title = req.params.title;
         const description = req.params.description;
         const instructor = req.params.instructor;
@@ -271,52 +254,52 @@ async function run() {
         const price = req.params.price;
         // console.log(title,description,instructor,img);
         const courseInfo = { title, description, instructor, img, price };
-        const result = await courseCollection.insertOne( courseInfo );
-        res.send( result );
+        const result = await courseCollection.insertOne(courseInfo);
+        res.send(result);
       }
     );
 
     // getting all courses
-    app.get( "/courses", async ( req, res ) => {
-      const courses = await courseCollection.find( {} ).toArray()
+    app.get("/courses", async (req, res) => {
+      const courses = await courseCollection.find({}).toArray()
       // const courses = await test.find({}).toArray()
-      res.send( courses );
-    } );
+      res.send(courses);
+    });
 
     // getting a single course by id
-    app.get( "/courses/:id", async ( req, res ) => {
+    app.get("/courses/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: ObjectId( id ) }
-      const result = await courseCollection.findOne( filter );
+      const filter = { _id: ObjectId(id) }
+      const result = await courseCollection.findOne(filter);
       // const result = await test.findOne(filter);
-      res.send( result );
-    } );
+      res.send(result);
+    });
 
-    app.delete( '/delete-course/:id', async ( req, res ) => {
+    app.delete('/delete-course/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: ObjectId( id ) }
-      const result = await courseCollection.deleteOne( filter );
+      const filter = { _id: ObjectId(id) }
+      const result = await courseCollection.deleteOne(filter);
       // const result = await test.deleteOne(filter);
-      res.send( result );
-    } )
+      res.send(result);
+    })
 
   } catch {
-    ( e ) => {
-      console.error( "error inside run function: ", e );
+    (e) => {
+      console.error("error inside run function: ", e);
     };
   }
 }
 
-run().catch( ( e ) => console.error( "run function error..", e ) );
+run().catch((e) => console.error("run function error..", e));
 
-app.get( "/", ( req, res ) => {
-  res.send( "Tech Quest server is running..." );
-} );
+app.get("/", (req, res) => {
+  res.send("Tech Quest server is running...");
+});
 
 app.listen(port, () => {
   console.log(`Tech Quest server is running on ${port}`);
 });
 
-app.listen( port, () => {
-  console.log( `Tech Quest server is running on ${ port }` );
-} );
+app.listen(port, () => {
+  console.log(`Tech Quest server is running on ${port}`);
+});
